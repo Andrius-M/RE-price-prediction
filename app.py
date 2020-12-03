e882f81345f051827834d37f120e9792aeac36db
Andrius-M <68550340+Andrius-M@users.noreply.github.com>
2020-12-03 22:39:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,5 @@ def predict_expenses():
     return render_template('index.html', prediction_text='Predicted price $ {}'.format(output))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
